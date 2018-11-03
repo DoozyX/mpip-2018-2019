8f3866f2994cb5a5e065e1f29682dfe684a4501a
Slobodan Kletnikov <slobodan_kletnikov@hotmail.com>
2018-11-03 21:15:08 +0100

    Add screens and initial activites

---
+++ lab/src/main/java/doozy/lab1/lab/ExplicitActivity.kt
@@ -0,0 +1,12 @@
+package doozy.lab1.lab
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExplicitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explicit)
+    }
+}
---
+++ lab/src/main/java/doozy/lab1/lab/ImplicitActivity.kt
@@ -0,0 +1,12 @@
+package doozy.lab1.lab
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ImplicitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_implicit)
+    }
+}
