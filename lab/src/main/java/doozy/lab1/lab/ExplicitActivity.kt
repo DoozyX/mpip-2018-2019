6961daa7219c695529f444ea20925c100ef4ad16
Slobodan Kletnikov <slobodan_kletnikov@hotmail.com>
2018-11-04 10:09:24 +0100

    Add first button functionality

--- lab/src/main/java/doozy/lab1/lab/ExplicitActivity.kt
+++ lab/src/main/java/doozy/lab1/lab/ExplicitActivity.kt
@@ -1,12 +1,34 @@
 package doozy.lab1.lab
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 
 class ExplicitActivity : AppCompatActivity() {
+    private lateinit var btnOk: Button
+    private lateinit var btnCancel: Button
+    private lateinit var etEnterText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explicit)
+
+        btnOk = findViewById(R.id.btn_ok)
+        btnOk.setOnClickListener {
+            val returnIntent = Intent()
+            returnIntent.putExtra("result", etEnterText.text.toString())
+            setResult(Activity.RESULT_OK, returnIntent)
+            finish()
+        }
+        btnCancel = findViewById(R.id.btn_cancel)
+        btnCancel.setOnClickListener {
+            val returnIntent = Intent()
+            setResult(Activity.RESULT_CANCELED, returnIntent)
+            finish()
+        }
+        etEnterText = findViewById(R.id.et_enter_text)
     }
 }
--- lab/src/main/java/doozy/lab1/lab/MainActivity.kt
+++ lab/src/main/java/doozy/lab1/lab/MainActivity.kt
@@ -1,12 +1,46 @@
 package doozy.lab1.lab
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+import java.util.logging.Logger
+
 
 class MainActivity : AppCompatActivity() {
+    private var logger: Logger = Logger.getLogger(MainActivity::class.java.name)
+
+    private lateinit var btnStartExplicitActivity: Button
+    private lateinit var tvResultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStartExplicitActivity = findViewById(R.id.btn_start_explicit_activity)
+        btnStartExplicitActivity.setOnClickListener {
+            openExplicitActivity()
+        }
+
+
+        tvResultText = findViewById(R.id.tv_result_text)
+    }
+
+    private fun openExplicitActivity() {
+        logger.warning("BUTTON CLICKED")
+        val explicitActivityIntent = Intent(this, ExplicitActivity::class.java)
+        startActivityForResult(explicitActivityIntent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                val result = data?.getStringExtra("result")
+                tvResultText.text = result
+            }
+        }
     }
 }
